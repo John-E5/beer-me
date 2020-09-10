45d75ebe33ea2eabea26372f672a21ceca6a7e39
John Ennis <johnennis5@gmail.com>
2020-09-10 08:55:32 +0100

    Added brewdog api and get script for beers

--- src/components/BeerChoices.vue
+++ src/components/BeerChoices.vue
@@ -1,14 +1,32 @@
 <template>
     <v-card>
-        <v-card-title></v-card-title>
+        <v-card-title>{{info}}</v-card-title>
         <v-card-subtitle></v-card-subtitle>
         <v-card-text></v-card-text>
     </v-card>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-    
+    name: 'BeerChoices',
+    data() {
+        return {
+            info: null,
+            errored: false
+        };
+    },
+    mounted() {
+        axios
+            .get('https://api.punkapi.com/v2/beers/random')
+            .then(response => {
+                (this.info = response.data);
+            })
+            .catch(error => {
+                console.log(error);
+                this.errored = true;
+            })
+    },
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
     <v-main>
         <HeroSection />
+        <BeerChoices/>
     </v-main>
 </template>
 
 <script>
 import HeroSection from '@/components/HeroSection.vue';
+import BeerChoices from '@/components/BeerChoices.vue'
 export default {
     name: 'Home',
     components: {
-        HeroSection
+        HeroSection,
+        BeerChoices
     }
 };
 </script>
