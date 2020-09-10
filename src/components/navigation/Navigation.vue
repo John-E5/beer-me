3c34fec901b74ebc521af9cccfe9067459ca9032
John Ennis <johnennis5@gmail.com>
2020-09-10 08:07:20 +0100

    Updated package.json and fixed linting errors

--- src/components/navigation/Navigation.vue
+++ src/components/navigation/Navigation.vue
@@ -1,7 +1,8 @@
 <template>
-    <v-navigation-drawer color="indigo darken-2" v-model="drawer" app>
+    <span>
+        <v-navigation-drawer color="indigo darken-2" v-model="drawer" app>
             <v-list dense>
-                <v-list-item link  to="/">
+                <v-list-item link to="/">
                     <v-list-item-action>
                         <v-icon>mdi-home</v-icon>
                     </v-list-item-action>
@@ -10,23 +11,23 @@
                     </v-list-item-content>
                 </v-list-item>
                 <v-list-item link to=”/contact”>
-                    <v-list-item-action>
-                        <v-icon>mdi-email</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title>Contact</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
+                <v-list-item-action>
+                    <v-icon>mdi-email</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title>Contact</v-list-item-title>
+                </v-list-item-content>
             </v-list>
         </v-navigation-drawer>
         <v-app-bar app color="indigo darken-4">
             <v-app-bar-nav-icon
                 @click.stop="drawer = !drawer"
             ></v-app-bar-nav-icon>
-            <v-toolbar-title>{{siteTitle}}</v-toolbar-title>
+            <v-toolbar-title>{{ siteTitle }}</v-toolbar-title>
 
             <v-spacer></v-spacer>
         </v-app-bar>
+    </span>
 </template>
 
 <script>
@@ -36,11 +37,9 @@ export default {
         return {
             siteTitle: 'Beer Me',
             drawer: null
-        }
-    },
-}
+        };
+    }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
