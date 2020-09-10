3049921796c42c390dda4f35ce665f1158e2dd46
John Ennis <johnennis5@gmail.com>
2020-09-10 07:58:58 +0100

    Added Navigation Drawer

--- src/components/navigation/Navigation.vue
+++ src/components/navigation/Navigation.vue
@@ -1,5 +1,24 @@
 <template>
-    
+    <v-navigation-drawer color="indigo darken-2" v-model="drawer" app>
+            <v-list dense>
+                <v-list-item link  to="/">
+                    <v-list-item-action>
+                        <v-icon>mdi-home</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Home</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item link to=”/contact”>
+                    <v-list-item-action>
+                        <v-icon>mdi-email</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Contact</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
 </template>
 
 <script>
