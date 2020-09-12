47a9411817ec6cc6515a95402971691644b91087
John Ennis <johnennis5@gmail.com>
2020-09-12 21:23:48 +0100

    fixed linting

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,18 +1,19 @@
 <template>
     <v-parallax
-    dark
-    src='https://industryeurope.com/downloads/4544/download/beer%20bottles.jpg?cb=65972db729acf95565681dc14e5118ea&w=1032'
-  >
-    <v-row
-      align="center"
-      justify="center"
+        dark
+        src="https://industryeurope.com/downloads/4544/download/beer%20bottles.jpg?cb=65972db729acf95565681dc14e5118ea&w=1032"
     >
-      <v-col class="text-center indigo--text" cols="12">
-        <h1 class="display-3 font-weight-black mb-4">Cant Decide on a Brewdog?</h1>
-        <h4 class="subheading display-1 font-weight-black">Get help with choosing your brew!.</h4>
-      </v-col>
-    </v-row>
-  </v-parallax>
+        <v-row align="center" justify="center">
+            <v-col class="text-center indigo--text" cols="12">
+                <h1 class="display-3 font-weight-black mb-4">
+                    Cant Decide on a Brewdog?
+                </h1>
+                <h4 class="subheading display-1 font-weight-black">
+                    Get help with choosing your brew!.
+                </h4>
+            </v-col>
+        </v-row>
+    </v-parallax>
 </template>
 
 <script>
@@ -21,6 +22,4 @@ export default {
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
